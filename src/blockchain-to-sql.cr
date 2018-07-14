6eedc145b287379e687e9f9380ffb8b49151e9af
John Doe <johndoe@example.com>
2018-07-14 10:19:28 +0100

    Download blockchain to SQL databases.

---
+++ src/blockchain-to-sql.cr
@@ -0,0 +1,6 @@
+require "./blockchain-to-sql/*"
+
+# TODO: Write documentation for `Blockchain::To::Sql`
+module Blockchain::To::Sql
+  # TODO: Put your code here
+end
---
+++ src/blockchain-to-sql/version.cr
@@ -0,0 +1,3 @@
+module Blockchain::To::Sql
+  VERSION = "0.1.0"
+end
